521a09f1e31dfcd69ee87c20da884d3c8abe435b
Matias <hola@mati.lol>
2026-03-12 19:58:14 -0300

    Migrate deployment from Cloudflare to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -10,10 +9,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), sitemap()],
-  adapter: cloudflare(),
-  server: {
-    allowedHosts: ["guaranteed-permits-alarm-expenditure.trycloudflare.com"],
-  },
 
   vite: {
     plugins: [tailwindcss()],
